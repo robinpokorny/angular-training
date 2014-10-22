f7d112059fd181502657e394ef8fcb3ab26ee324
Robin Pokorný <robin@medvedi.eu>
2014-10-22 18:46:16 +0200

    Add pokemons list

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,17 +6,55 @@
     .controller('MainController', MainController);
 
   function MainController() {
-    var vm = this,
-      counter = 0;
-    vm.titleList = ['Kurzy', 'Školení', 'Přednášky', 'Wokshopy'];
-    vm.next = next;
+    var vm = this;
 
-    next();
-
-    function next() {
-      vm.title = vm.titleList[counter % vm.titleList.length];
-      counter++;
-    }
   }
+
+  var pokemons = [
+    {
+      "name": "rattata",
+      "resource_uri": "api/v1/pokemon/19/"
+    },
+    {
+      "name": "charmander",
+      "resource_uri": "api/v1/pokemon/4/"
+    },
+    {
+      "name": "charmeleon",
+      "resource_uri": "api/v1/pokemon/5/"
+    },
+    {
+      "name": "wartortle",
+      "resource_uri": "api/v1/pokemon/8/"
+    },
+    {
+      "name": "blastoise",
+      "resource_uri": "api/v1/pokemon/9/"
+    },
+    {
+      "name": "caterpie",
+      "resource_uri": "api/v1/pokemon/10/"
+    },
+    {
+      "name": "metapod",
+      "resource_uri": "api/v1/pokemon/11/"
+    },
+    {
+      "name": "butterfree",
+      "resource_uri": "api/v1/pokemon/12/"
+    },
+    {
+      "name": "spearow",
+      "resource_uri": "api/v1/pokemon/21/"
+    },
+    {
+      "name": "kakuna",
+      "resource_uri": "api/v1/pokemon/14/"
+    },
+    {
+      "name": "beedrill",
+      "resource_uri": "api/v1/pokemon/15/"
+    }
+  ]
 
 }());
